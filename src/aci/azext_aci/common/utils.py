8f0675a38e82591b828c2a0aed3f363d74f8e8ae
Mitesh Shah <mitsha@microsoft.com>
2019-12-24 11:19:33 +0530

    Adding Utils for Singleton

---
+++ src/aci/azext_aci/common/utils.py
@@ -0,0 +1,7 @@
+def singleton(myclass):
+    instance = [None]
+    def wrapper(*args, **kwargs):
+        if instance[0] is None:
+            instance[0] = myclass(*args, **kwargs)
+        return instance[0]
+    return wrapper 
